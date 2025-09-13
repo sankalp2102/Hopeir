27ec7124158fa04d068450ea1c82b89ba3ae55d2
Sankalp Gupta <sankalpgupta444@gmail.com>
2025-09-13 16:56:57 +0530

    Changes in email api url

--- Hopeir/core/urls.py
+++ Hopeir/core/urls.py
@@ -3,7 +3,7 @@ from .views import (ProfileViewByEmail, ProfileViewByUserId,
                     ProfileCreateView, VehicleProfileListCreateAPIView, 
                     TestAPIView, DeleteUserByEmailView)
 urlpatterns = [
-    path('profile/<int:user_id>/', ProfileViewByUserId.as_view(), name='profile-get-user-id'),
+    path('profiles/<str:user_id>/', ProfileViewByUserId.as_view(), name='profile-get-user-id'),
     path('profile/<str:email>/', ProfileViewByEmail.as_view(), name='profile-get-update-email'),
     path('vehicles/', VehicleProfileListCreateAPIView.as_view(), name='vehicles-by-user'),
     path('profile/create/', ProfileCreateView.as_view(), name='profile-create'),
