1d60894e885478cae369765b877d02649678eb2e
Sankalp Gupta <sankalpgupta444@gmail.com>
2026-06-03 01:08:30 +0530

    Test auto deploy pipeline

--- Hopeir/Hopeir/urls.py
+++ Hopeir/Hopeir/urls.py
@@ -30,3 +30,4 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
